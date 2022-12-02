5cc6677534608f3b3a1b75c06487b5ad33bab5ec
oba <obatirou@gmail.com>
2022-12-02 18:06:28 +0100

    WIP: 25/151
    TODO
    * finish implem
    * refactor
    * git commits

--- python/evm.py
+++ python/evm.py
@@ -13,6 +13,11 @@
 import json
 import os
 
+UINT256_MAX = 115792089237316195423570985008687907853269984665640564039457584007913129639935
+
+def to_decimal(op):
+    return int(op, 16)
+
 def evm(code):
     pc = 0
     success = True
@@ -20,11 +25,84 @@ def evm(code):
 
     while pc < len(code):
         op = code[pc]
+        if op == to_decimal("00"):
+            break
+        if op == to_decimal("60"):
+            pc += 1
+            stack.append(code[pc])
+        if op == to_decimal("61"):
+            pc += 1
+            stack.append(to_decimal(code[pc:pc+2].hex()) )
+            pc += 2
+        if op == to_decimal("63"):
+            pc += 1
+            stack.append(to_decimal(code[pc:pc+4].hex()) )
+            pc += 4
+        if op == to_decimal("65"):
+            pc += 1
+            stack.append(to_decimal(code[pc:pc+6].hex()) )
+            pc += 6
+        if op == to_decimal("69"):
+            pc += 1
+            stack.append(to_decimal(code[pc:pc+10].hex()) )
+            pc += 10
+        if op == to_decimal("6A"):
+            pc += 1
+            stack.append(to_decimal(code[pc:pc+11].hex()) )
+            pc += 11
+        if op == to_decimal("7F"):
+            pc += 1
+            stack.append(to_decimal(code[pc:pc+32].hex()) )
+            pc += 31
+        if op == to_decimal("50"):
+            stack.pop()
+        if op == to_decimal("01"):
+            value = stack.pop() + stack.pop()
+            if value > UINT256_MAX:
+                value -= UINT256_MAX + 1
+            stack.append(value)
+        if op == to_decimal("02"):
+            value = stack.pop() * stack.pop()
+            if value > UINT256_MAX:
+                value -= UINT256_MAX + 1
+            stack.append(value)
+        if op == to_decimal("03"):
+            value = stack.pop() - stack.pop()
+            if value < 0:
+                value += UINT256_MAX + 1
+            stack.append(value)
+        if op == to_decimal("04"):
+            a = stack.pop()
+            b = stack.pop()
+            if b == 0:
+                value = 0
+            else:
+                value = a // b
+            stack.append(value)
+        if op == to_decimal("06"):
+            a = stack.pop()
+            b = stack.pop()
+            if b == 0:
+                value = 0
+            else:
+                value = a % b
+            stack.append(value)
+        if op == to_decimal("08"):
+            a = stack.pop()
+            b = stack.pop()
+            c = stack.pop()
+            value = a + b
+            if value > UINT256_MAX:
+                value -= UINT256_MAX + 1
+            if c == 0:
+                value = 0
+            else:
+                value %= c
+            stack.append(value)
         pc += 1
 
-        # TODO: implement the EVM here!
-        
-
+    if stack:
+        stack.reverse()
     return (success, stack)
 
 def test():
